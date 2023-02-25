34e50af9da81106babbd2b45faf6b72289c696dc
unknown <Andrew M>
2023-02-25 15:24:23 +0300

    the second task is done

--- lesson2.py
+++ lesson2.py
@@ -5,13 +5,13 @@
 # Input: 5
 # Output: 120
 
-n = int(input("Enter your positive number over 0:  "))
-count = 1
-factorial = 1
-while count <= n:
-    factorial *= count
-    count += 1
-print(factorial)
+# n = int(input("Enter your positive number over 0:  "))
+# count = 1
+# factorial = 1
+# while count <= n:
+#     factorial *= count
+#     count += 1
+# print(factorial)
 
 
 # Дано натуральное число A > 1. 
@@ -20,3 +20,36 @@ print(factorial)
 # что φ(n)=A. Если А не является числом Фибоначчи, выведите число -1.
 # Input: 5
 # Output: 6
+
+def fibo(number):
+    start = 1
+    next = 1
+    counter = 4
+
+    while True:
+        current = start + next
+        if current == number:
+            return counter
+        if current > number:
+            return -1
+        start = next
+        next = current
+        counter += 1
+
+
+a = int(input("Enter your positive number over 1:  "))
+print(fibo(a))
+
+# My decision
+#  a = int(input("Enter your positive number over 1:  "))
+# count = 1
+# fiabA = 0
+# fiabB = 1
+# while fiabB <= a:
+#     fiabC = fiabA + fiabB
+#     fiabA = fiabB
+#     fiabB = fiabC
+#     count += 1
+#     if fiabB != a:
+#         print(-1)
+# print(count)
